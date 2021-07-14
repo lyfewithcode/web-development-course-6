f08e675efc3b24b9814cf59504cba4027a5278d3
lyfewithcode <lyfewithcode@gmail.com>
2021-07-14 22:47:10 +0700

    Print statement

--- echo-and-print.php
+++ echo-and-print.php
@@ -1,5 +1,7 @@
 <?php
     echo "My name is Yuda<br>";
     echo "I like cheese<br>";
-    echo 'I also like single quotes'
+    echo 'I also like single quotes';
+    print "I like echo more than print";
+    print 'I like echo more than print';
 ?>
