68c09146a5d3320064670e9fc487f68ce02b7642
lyfewithcode <lyfewithcode@gmail.com>
2021-07-14 23:06:18 +0700

    Writing html inside php

---
+++ html-output-inside-php.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    .myclass {
+        color: red;
+    }
+    </style>
+</head>
+<body>
+    
+<?php
+    echo "<h1 class=\"myclass\">My Heading</h1>";
+    echo "<br>";
+    echo "<input type=\"text\" id=\"first-name\" value=\"John\">";
+?>
+
+</body>
+</html>
