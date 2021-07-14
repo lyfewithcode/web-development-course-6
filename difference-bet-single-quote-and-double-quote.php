f5e97859f8719d67564a0e3f6f9d6b209b2f7dd2
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 00:43:58 +0700

    Important concept: Difference single double quote

---
+++ difference-bet-single-quote-and-double-quote.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $var1 = "My name is Yuda";
+    $var2 = "I like to make friends";
+    
+    echo $var1.'.'.$var2;
+
+    $friend1 = "John";
+    $friend2 = "Bob";
+
+    echo "<br>";
+    echo "My best friend are $friend1 and $friend2";
+    echo "<br>";
+    echo 'My best friend are '.$friend1.' and '.$friend2;
+    echo "<br>";
+    echo 'My best friend are $friend1 and $friend2';
+?>
+
+</body>
+</html>
