99163e9a70b422a90719370d49a89a19b869760b
lyfewithcode <lyfewithcode@gmail.com>
2021-07-14 23:20:36 +0700

    Php comments

---
+++ comments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+// this is a variable
+    $name = "Bob";
+/* Lorem ipsum bla bla bla
+bla bla bla */
+?>
+
+<input type="text" id="name" value="<?php echo $name; ?>">
+</body>
+</html>
