c3c762918fae7753d3c54b3bc7b1440567cc9359
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 00:59:28 +0700

    Arithmetic operators

---
+++ arithmetic-operators.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+   $x = 5;
+   $y = 13;
+   
+   echo $x + $y; // addition
+   echo "<br>";
+   echo $y - $x; // subtraction
+   echo "<br>";
+   echo $x * $y; // multiplication
+   echo "<br>";
+   echo $x / $y; // division
+   echo "<br>";
+   echo $y % $x; // modulus
+   echo "<br>";
+   echo 2**3; // exponential
+
+?>
+
+</body>
+</html>
