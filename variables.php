b3600bb85e10d3bbb000cb3d4bcd7844cccdb38c
lyfewithcode <lyfewithcode@gmail.com>
2021-07-14 23:31:40 +0700

    Php variables

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $myName = "John";
+    echo $myName;
+    $myNumber = 5.5;
+    echo "<br>";
+    echo $myNumber;
+    $myBoolean = true;
+?>
+
+</body>
+</html>
