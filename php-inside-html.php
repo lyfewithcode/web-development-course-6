70ed14ea552d92889fe344b39bb935117f4f05b8
lyfewithcode <lyfewithcode@gmail.com>
2021-07-14 23:15:27 +0700

    Writing php inside html

---
+++ php-inside-html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    .myclass {
+        color: red;
+    }
+    </style>
+</head>
+<body>
+    
+<?php
+    $name = "Bob";
+?>
+
+<input type="text" id="name" value="<?php echo $name; ?>">
+</body>
+</html>
