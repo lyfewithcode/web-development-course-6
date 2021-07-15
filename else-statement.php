1e636d685c9e87f27fc8f33ff154129e061db065
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 10:37:48 +0700

    Else statement

---
+++ else-statement.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $x = 5;
+    $y = "5";
+    $z = 10;
+    if($x == $z) {
+        echo 'Yup, both are having same values<br>';
+    } else {
+        echo "No, they are not same<br>";
+    }
+
+    if(1) {
+        echo 'Hi, in programming 1 means true and hence if its checked in a condition, it will always execute<br>';
+    }
+
+    if(true) {
+        echo 'Hi, I will execute properly';
+    }
+    
+?>
+
+</body>
+</html>
