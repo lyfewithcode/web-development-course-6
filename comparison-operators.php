9d917907e9b77bab13a85f849820b1a8254a95d3
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 09:16:23 +0700

    Comparison operators

---
+++ comparison-operators.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    // equal ==
+    // triple equal ===
+    // not equal !=
+    // not equal part 2 <>
+    // not equal equal !==
+    // >, <, >=, <=
+
+?>
+
+</body>
+</html>
