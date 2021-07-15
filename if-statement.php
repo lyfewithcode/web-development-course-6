2025455bd17cfbf942c2fffc6517e15e9eac62d4
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 10:28:08 +0700

    If statement

---
+++ if-statement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $x = 5;
+    $y = "5";
+    if($x == $y) {
+        echo 'Yup, both are having same values<br>';
+    }
+    if($x !== $y) {
+        echo 'No, they are not identical';
+    }
+?>
+
+</body>
+</html>
