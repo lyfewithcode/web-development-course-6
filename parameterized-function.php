96cdf8abdb655199ad80c9e942be573c936b4a77
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 13:27:10 +0700

    Parameterized function

---
+++ parameterized-function.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    function myFunction($name, $age) {
+        // echo "My name is $name and my age is $age <br>";
+        echo 'My name is'.$name.' and my age is '.$age.'<br>';
+    }
+
+    myFunction('Yuda', 20);
+    myFunction('John', 23);
+    myFunction('Bob', 19);
+?>
+
+</body>
+</html>
