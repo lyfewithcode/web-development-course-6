c422bad739a47749237e925baa12d6ec3a85ede4
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 14:18:00 +0700

    Return function

---
+++ return-function.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    function add($x, $y) {
+        return $x + $y;
+    }
+
+    echo $z = add(5, 10).'<br>';
+    echo $z = add(15, 10).'<br>';
+    echo $z = add(25, 10).'<br>';
+?>
+
+</body>
+</html>
