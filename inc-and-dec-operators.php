4538a47488faf362df19d1e8afea1e35c5708a88
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 09:35:48 +0700

    Increment and decrement operators

---
+++ inc-and-dec-operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $x = 5;
+    // $x ++; // $x = $x + 1;
+    // echo $x;
+    echo $x ++; // $x ++ return the value of x and then it increments , but ++ $x it first increments and then it returns the value
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $x --;
+    echo "<br>";
+    echo $x;
+    
+
+?>
+
+</body>
+</html>
