124f257df989a7cea79b86b2f6b746fdadbf64a7
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 09:08:38 +0700

    Assignment operators

---
+++ assignment operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+   $x = 5;
+   $y = 13;
+
+   $z = $x + $y;
+//    $x = $x + $y;
+   $x += $y;
+   $x -= $y; // $x = $x - $y;
+   $x /= 5; // $x = $x / 5;
+   $x *= 5; // $x = $x * 5;
+   $x %= 3;
+   echo $x;
+
+?>
+
+</body>
+</html>
