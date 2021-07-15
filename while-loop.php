025387beb1faf41851c45eb0896b0887c0206fad
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 12:25:10 +0700

    While loop

---
+++ while-loop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $x = 1;
+    while($x <= 10) {
+        echo 'Hi <br>';
+        // $x ++;
+        // $x = x + 2;
+        $x += 2;
+    } 
+
+?>
+
+</body>
+</html>
