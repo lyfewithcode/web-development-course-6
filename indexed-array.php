1a61497ea8f117959eaca8be8b9545e77a03523e
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 14:28:59 +0700

    Introduction to array and indexed array

---
+++ indexed-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $myArray = array("Yuda", "Raka", "Devi");
+    print_r($myArray);
+    echo '<br>';
+    echo $myArray[0];
+?>
+
+</body>
+</html>
