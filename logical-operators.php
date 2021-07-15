d4e91aa0f014438a07a86ec8a2a6153f8f80084e
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 09:20:49 +0700

    Logical operators

---
+++ logical-operators.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    // and, or, &&, ||, xor, ! are logical operators in PHP
+
+?>
+
+</body>
+</html>
