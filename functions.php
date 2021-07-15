f67b7e5b5ca2cc64d407cbc5a72aea97d287c9da
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 13:18:50 +0700

    Function in php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    function myFunction() {
+        echo "My name is Yuda";
+    }
+
+    myFunction();
+?>
+
+</body>
+</html>
