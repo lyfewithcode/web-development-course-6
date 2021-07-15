d56c57d11d726c9b5a4dcc212121d08abfb3abae
lyfewithcode <lyfewithcode@gmail.com>
2021-07-15 09:44:20 +0700

    String operators

---
+++ string-operators.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style type="text/css">
+    
+    </style>
+</head>
+<body>
+    
+<?php
+    $var1 = "John";
+    $var1.= " Hernandez"; //concatenation operators, add 2 strings
+    echo $var1;
+
+    // . , .= are string operators
+?>
+
+</body>
+</html>
